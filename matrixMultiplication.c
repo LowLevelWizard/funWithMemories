c35df516727f8b59c4b24c31da8e766435ef974b
LowLevelWizard <LowLevelWizard>
2024-08-16 11:40:04 -0400

    Fixed matrix 2

--- matrixMultiplication.c
+++ matrixMultiplication.c
@@ -9,23 +9,40 @@ int main(){
     printf("Matrix One columns: ");
     scanf("%d", &colOne);
     //Intake mat 2 info
-    printf("Matrix One rows: ");
-    scanf("%d", &rowOne);
-    printf("Matrix One columns: ");
-    scanf("%d", &colOne);
+    printf("Matrix Two rows: ");
+    scanf("%d", &rowTwo);
+    printf("Matrix Two columns: ");
+    scanf("%d", &colTwo);
 
     if(colOne != rowTwo){
         printf("Invalid Matrices\n");
         return 1;
     }
 
-    int* rowOnePtr = (int*)malloc(rowOne*sizeof(int));
-    int* colOnePtr = (int*)malloc(colOne*sizeof(int));
+    int* matOnePtr = (int*)malloc(colOne*rowOne*sizeof(int));
 
-    int* rowOnePtr = (int*)malloc(rowOne*sizeof(int));
-    int* colOnePtr = (int*)malloc(colOne*sizeof(int));
+    int* matTwoPtr = (int*)malloc(colTwo*rowTwo*sizeof(int));
+
+    int* resultMat = (int*)malloc(rowOne*colTwo*sizeof(int));
 
     //TODO:
     //Finish this mess
+
+    //For loop?
+    //Matrix One input:
+    int i, j;
+    for(i = 0; i < colOne; i++){
+        for(j = 0; j < rowOne; j++){
+            printf("Matrix One: Input row %d, column %d: ", i, j);
+            scanf("%d",matOnePtr+i+j);
+            //printf("\n");
+        }
+    }
+
+    //Not working for some reason:
+    i = 0;
+    for(i=0; i < colOne*rowOne; i++){
+        printf("%d", *(matOnePtr+i));
+    }
 
 }
