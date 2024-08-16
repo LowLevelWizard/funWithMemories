cad7ad09e8329bb0af6e1d87bee9573f2fae0a32
LowLevelWizard <LowLevelWizard>
2024-08-16 14:56:04 -0400

    Added dot protuct function

--- matrixMultiplication.c
+++ matrixMultiplication.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int dotProduct(int* vector1, int* vector2, int length);
+
 int main(){
     int rowOne, colOne, rowTwo, colTwo;
     //Intake mat 1 info
@@ -13,11 +15,13 @@ int main(){
     scanf("%d", &rowTwo);
     printf("Matrix Two columns: ");
     scanf("%d", &colTwo);
-
+    
+    /*
     if(colOne != rowTwo){
         printf("Invalid Matrices\n");
         return 1;
     }
+    */
 
     int* matOnePtr = (int*)malloc(colOne*rowOne*sizeof(int));
 
@@ -34,15 +38,39 @@ int main(){
     for(i = 0; i < colOne; i++){
         for(j = 0; j < rowOne; j++){
             printf("Matrix One: Input row %d, column %d: ", i, j);
-            scanf("%d",matOnePtr+i+j);
+            scanf("%d",matOnePtr+(i*rowOne)+j);
             //printf("\n");
         }
     }
 
-    //Not working for some reason:
-    i = 0;
-    for(i=0; i < colOne*rowOne; i++){
-        printf("%d", *(matOnePtr+i));
+    //Works now:
+    /*
+    for(i=0; i < colOne; i++){
+        for(j=0; j < rowOne; j++){
+            printf(" %d ", *(matOnePtr+i));
+        }
+    }
+    */
+
+   for(i = 0; i < colTwo; i++){
+        for(j = 0; j < rowTwo; j++){
+            printf("Matrix One: Input row %d, column %d: ", i, j);
+            scanf("%d",matTwoPtr+(i*rowTwo)+j);
+            //printf("\n");
+        }
     }
 
+    printf("Loading now \n");
+
+
+
+}
+
+
+int dotProduct(int* vector1, int* vector2, int length){
+    int i, sum = 0;
+    for(i = 0; i < length; i++){
+        sum = *(vector1+i)+*(vector2+i);
+    }
+    return sum;
 }
